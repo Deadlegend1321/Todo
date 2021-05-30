0eaaeb9c18657b70cfc2e9b78152a3eb5dcfea75
Deadlegend1321 <mudit1318@outlook.com>
2021-05-30 16:41:50 +0530

    Navigation Components and Transition Animation

--- app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
@@ -1,11 +1,12 @@
 package com.example.todo.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.fragment.findNavController
 import com.example.todo.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +36,22 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        var floatingActionButton : FloatingActionButton? = null
+        floatingActionButton = view.findViewById(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        view.findViewById<ConstraintLayout>(R.id.listLayout).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
     companion object {
--- app/src/main/java/com/example/todo/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todo/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todo.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.todo.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -34,8 +32,14 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
     companion object {
