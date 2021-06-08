c478603ee7b546da1699ace98431ca7ef70ada13
Deadlegend1321 <mudit1318@outlook.com>
2021-06-08 20:43:28 +0530

    RecyclerView, Updating and deleting data

--- app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
@@ -1,12 +1,21 @@
 package com.example.todo.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo.R
+import com.example.todo.data.ToDoViewModel
+import com.example.todo.fragments.SharedViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +32,12 @@ class ListFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
+    private val adapter: ListAdapter by lazy {
+        ListAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -37,21 +52,62 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer {
+            data -> mSharedViewModel.checkIfDatabaseEmpty(data)
+            adapter.setData(data)
+        })
+        mSharedViewModel.emptyDatabase.observe(viewLifecycleOwner, Observer {
+            showEmptyDatabaseViews(it)
+        })
+
         var floatingActionButton : FloatingActionButton? = null
         floatingActionButton = view.findViewById(R.id.floatingActionButton)
         floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
-        view.findViewById<ConstraintLayout>(R.id.listLayout).setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
-        }
+
 
         setHasOptionsMenu(true)
         return view
     }
 
+    private fun showEmptyDatabaseViews(emptyDatabase: Boolean) {
+        if (emptyDatabase){
+            view?.no_data_imageView?.visibility = View.VISIBLE
+            view?.no_data_textView?.visibility = View.VISIBLE
+        }
+        else{
+            view?.no_data_imageView?.visibility = View.INVISIBLE
+            view?.no_data_textView?.visibility = View.INVISIBLE
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete_all){
+            confirmRemoval()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun confirmRemoval() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){ _, _ ->
+            mToDoViewModel.deleteAll()
+            Toast.makeText(requireContext(), "Removed Everything", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No"){ _, _ -> }
+        builder.setTitle("Delete Everything")
+        builder.setMessage("Are you sure?")
+        builder.create().show()
     }
 
     companion object {
